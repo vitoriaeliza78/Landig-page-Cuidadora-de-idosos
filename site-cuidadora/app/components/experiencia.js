78ce8de02bf2fe8f17818a3f813062664a54134f
vitoriaeliza78 <vitoria69796@estudante.ifb.edu.br>
2025-12-19 17:26:22 -0300

    experiencia

--- site-cuidadora/app/components/experiencia.js
+++ site-cuidadora/app/components/experiencia.js
@@ -1,6 +1,13 @@
+
+
 export default function Experiencia() {
     return(
         <section id="experiencia" className=" bg-[#122432]">
+            <div className="container mx-auto w-full h-full flex justify-center items-center">
+                <h1 className="font-texto text-4xl md:text-5xl text-white text-center py-13">
+                    Experiência
+                </h1>
+            </div>
             
         </section>
 );
